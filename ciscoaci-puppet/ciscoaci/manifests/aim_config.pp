77bfb688b61e3156a9231f87ac0599ede89b3b8a
Ratnakar Kolli <rkolli@noironetworks.com>
2017-10-23 13:56:28 -0700

    provide option for scope_names and scope_infra

--- ciscoaci-puppet/ciscoaci/manifests/aim_config.pp
+++ ciscoaci-puppet/ciscoaci/manifests/aim_config.pp
@@ -11,6 +11,8 @@ class ciscoaci::aim_config(
   $aci_apic_aep,
   $aci_vpc_pairs = undef,
   $aci_opflex_vlan_range = '',
+  $aci_scope_names = 'False',
+  $aci_scope_infra = 'False',
 ) inherits ::ciscoaci::params
 {
 
@@ -42,7 +44,7 @@ class ciscoaci::aim_config(
      'apic/apic_password':                        value => $aci_apic_password;
      'apic/apic_use_ssl':                         value => True;
      'apic/verify_ssl_certificate':               value => False;
-     'apic/scope_names':                          value => False;
+     'apic/scope_names':                          value => $aci_scope_names;
      'aim/aim_system_id':                         value => $aci_apic_systemid;
   }  
 
@@ -50,7 +52,7 @@ class ciscoaci::aim_config(
      'DEFAULT/apic_system_id':                    value => $aci_apic_systemid;
      "apic_vmdom:$aci_apic_systemid/encap_mode":  value => $aci_encap_mode;
      'apic/apic_entity_profile':                  value => $aci_apic_aep;
-     'apic/scope_infra':                          value => False;
+     'apic/scope_infra':                          value => $aci_scope_infra;
      'apic/apic_provision_infra':                 value => False;
      'apic/apic_provision_hostlinks':             value => False;
   }
