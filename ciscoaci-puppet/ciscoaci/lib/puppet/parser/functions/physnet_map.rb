163ca307ee329e5c56f78b6f661ee07b8912478b
Ratnakar Kolli <rkolli@noironetworks.com>
2017-11-13 15:47:12 -0800

    remove the unwanted physnet configuration as per the new driver

--- ciscoaci-puppet/ciscoaci/lib/puppet/parser/functions/physnet_map.rb
+++ ciscoaci-puppet/ciscoaci/lib/puppet/parser/functions/physnet_map.rb
@@ -2,13 +2,14 @@ module Puppet::Parser::Functions
   newfunction(:physnet_map, :type => :rvalue) do |args|
     hosts = args[0]
     physmap = args[1]
+    domain = args[2]
     hosts.each do |hname|
       physmap.each do |physnetmap|
         ml = physnetmap.split(':')
         physnet = ml[0]
         intf = ml[1]
-        cmd = "/bin/aimctl manager host-link-network-label-delete #{hname} #{physnet} #{intf}"
-        cmdc = "/bin/aimctl manager host-link-network-label-create #{hname} #{physnet} #{intf}"
+        cmd = "/bin/aimctl manager host-link-network-label-delete #{hname}.#{domain} #{physnet} #{intf}"
+        cmdc = "/bin/aimctl manager host-link-network-label-create #{hname}.#{domain} #{physnet} #{intf}"
         system(cmd)
         system(cmdc)
       end
--- ciscoaci-puppet/ciscoaci/manifests/physdom.pp
+++ ciscoaci-puppet/ciscoaci/manifests/physdom.pp
@@ -5,9 +5,9 @@ define ciscoaci::physdom(
   $pnet = $pnet_l[0]
   $physdom = sprintf("pdom_%s", $pnet)
   aimctl_config {
-    "apic_physical_network:$pnet/hosts": value => $hosts;
-    "apic_physical_network:$pnet/segment_type": value => 'vlan';
-    "apic_physdom:$physdom/#dummy": value => 'dummy';
+    #"apic_physical_network:$pnet/hosts": value => $hosts;
+    #"apic_physical_network:$pnet/segment_type": value => 'vlan';
+    "apic_physdom:$physdom/encap_mode": value => 'vlan';
   }
 }
 
