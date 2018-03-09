8e1506425c0624ea615104d0e322bb8108875456
Ratnakar Kolli <rkolli@noironetworks.com>
2018-03-08 23:14:14 -0800

    support multiple links for physnet mapping

--- ciscoaci-puppet/ciscoaci/lib/puppet/parser/functions/physnet_map.rb
+++ ciscoaci-puppet/ciscoaci/lib/puppet/parser/functions/physnet_map.rb
@@ -7,11 +7,14 @@ module Puppet::Parser::Functions
       physmap.each do |physnetmap|
         ml = physnetmap.split(':')
         physnet = ml[0]
-        intf = ml[1]
-        cmd = "/bin/aimctl manager host-link-network-label-delete #{hname}.#{domain} #{physnet} #{intf}"
-        cmdc = "/bin/aimctl manager host-link-network-label-create #{hname}.#{domain} #{physnet} #{intf}"
-        system(cmd)
-        system(cmdc)
+        intfs = ml.drop(1)
+        puts intfs
+        intfs.each do |intf|
+           cmd = "/bin/aimctl manager host-link-network-label-delete #{hname}.#{domain} #{physnet} #{intf}"
+           cmdc = "/bin/aimctl manager host-link-network-label-create #{hname}.#{domain} #{physnet} #{intf}"
+           system(cmd)
+           system(cmdc)
+        end
       end
     end
   end
