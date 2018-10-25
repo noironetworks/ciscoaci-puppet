e256f27983f712fec55f00f56a93aac03c6c3ccc
Ratnakar Kolli <rkolli@noironetworks.com>
2018-10-25 12:16:56 -0700

    changes to support openshift on openstack

--- ciscoaci-puppet/ciscoaci/manifests/setup_dhclient_file.pp
+++ ciscoaci-puppet/ciscoaci/manifests/setup_dhclient_file.pp
@@ -1,11 +1,11 @@
-   define ciscoaci::setup_dhclient_file($real_opflex_uplink_iface) {
+   define ciscoaci::setup_dhclient_file($interface_name, $opflex_uplink_iface) {
      #$searchstr = "macaddress_${real_opflex_uplink_iface}"
      #$macaddr = inline_template("<%= scope.lookupvar(@searchstr) %>")
      #$macaddr = generate("/bin/facter macaddress_$real_opflex_uplink_iface")
      #$macaddr = generate("/bin/cat", "/sys/class/net/$real_opflex_uplink_iface/address")
 
       if($::osfamily == 'Redhat') {
-        $cmdstr = "/bin/bash -c '_xyz=`/bin/cat /sys/class/net/${real_opflex_uplink_iface}/address`; printf \"send dhcp-client-identifier 01:%s;\" \$_xyz > /etc/dhcp/dhclient-${real_opflex_uplink_iface}.conf' "
+        $cmdstr = "/bin/bash -c '_xyz=`/bin/cat /sys/class/net/${opflex_uplink_iface}/address`; printf \"send dhcp-client-identifier 01:%s;\" \$_xyz > /etc/dhcp/dhclient-${interface_name}.conf' "
 
         exec {'dhclient-file':
           command => $cmdstr,
